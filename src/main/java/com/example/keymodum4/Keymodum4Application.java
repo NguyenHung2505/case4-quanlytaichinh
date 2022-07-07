b02e1a035478fdaa88ee598a3c08abe49248560d
chandoithatsu <100838893+Afdfghjhgcfghvh@users.noreply.github.com>
2022-07-07 11:41:59 +0700

    taoduan

---
+++ src/main/java/com/example/keymodum4/Keymodum4Application.java
@@ -0,0 +1,13 @@
+package com.example.keymodum4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Keymodum4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Keymodum4Application.class, args);
+    }
+
+}
