eb3b68bc866c9c1c546683912029b02db4880d56
TangThiHoa <hoarua592001@gmail.com>
2022-07-08 16:28:55 +0700

    hoa xong dang ki dang nhap chuyen trang nhung bi load

--- src/main/resources/corona-free-dark-bootstrap-admin-template-1.0.0/template/pages/javascript/register.js
+++ src/main/resources/corona-free-dark-bootstrap-admin-template-1.0.0/template/pages/javascript/register.js
@@ -1,9 +1,9 @@
 function singUp() {
-    let userName = document.getElementById("username").value;
+    let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     let confirmPassword = document.getElementById("confirmPassword").value;
     let user = {
-        username: userName,
+        username: username,
         password: password,
         confirmPassword: confirmPassword
     }
@@ -14,13 +14,13 @@ function singUp() {
         },
         type: "POST",
         url: "http://localhost:8086/register",
+        dataType: "json",
         data: JSON.stringify(user),
         success: function () {
+            alert("Đăng kí thành công!")
             $('#username').val("");
             $('#password').val("");
             $('#confirmPassword').val("");
-            alert("Đăng kí thành công!")
-
         },
         error: function (error) {
             console.log(error)
