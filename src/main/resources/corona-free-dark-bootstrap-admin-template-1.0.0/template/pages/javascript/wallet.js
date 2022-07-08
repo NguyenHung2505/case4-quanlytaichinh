29c427d1641db280b3aeb0f0ff736f3ee08ebcc8
doanthihuong <doanthihuong>
2022-07-08 11:48:49 +0700

    done -hien-thi-wallet

---
+++ src/main/resources/corona-free-dark-bootstrap-admin-template-1.0.0/template/pages/javascript/wallet.js
@@ -0,0 +1,33 @@
+let show = document.getElementById("display");
+function showAllWallets() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8086/wallets",
+        success: function (data) {
+            display(data)
+        }, error: function (error) {
+            console.log(error);
+        }
+    })
+}
+function display(data) {
+    var content =``;
+    for (let i = 0; i < data.length; i++) {
+        content += getWallet(data[i])
+    }
+    document.getElementById("display").innerHTML = content;
+}
+
+function getWallet(item) {
+    return `<tr>
+            <td> ${item.id}</td>
+            <td> ${item.name}</td>
+            <td> ${item.icon}</td>
+            <td> ${item.moneyAmount}</td>
+            <td> ${item.moneyType.name}</td>
+            <td> ${item.user.username}</td>
+        </tr>`
+
+}
+
+
